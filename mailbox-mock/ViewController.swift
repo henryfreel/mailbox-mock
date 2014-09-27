e89b8d4b7ee357a1a50a6fc2d33ff8c4e148f56b
henryfreel <Henry@Henrys-MacBook-Pro-2.local>
2014-09-27 14:29:15 -0700

    All Animations inside ScrollView

--- mailbox-mock/ViewController.swift
+++ mailbox-mock/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var mailImage: UIImageView!
     @IBOutlet weak var feedImage: UIImageView!
     @IBOutlet weak var myScrollView: UIScrollView!
+    @IBOutlet weak var laterIconImage: UIImageView!
+    @IBOutlet weak var listIconImage: UIImageView!
+    @IBOutlet weak var archiveIconImage: UIImageView!
+    @IBOutlet weak var deleteIconImage: UIImageView!
     
     var imageCenter: CGPoint!
 
@@ -25,7 +29,6 @@ class ViewController: UIViewController {
     var brown = UIColor(red: 0.84, green: 0.64, blue: 0.47, alpha: 1.0)
     var green = UIColor(red: 0.45, green: 0.84, blue: 0.40, alpha: 1.0)
     var red = UIColor(red: 0.91, green: 0.33, blue: 0.23, alpha: 1.0)
-   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,40 +61,58 @@ class ViewController: UIViewController {
         } else if gestureRecognizer.state == UIGestureRecognizerState.Changed {
             
             
-            
             mailImage.center.x = translation.x + imageCenter.x
             var position = mailImage.frame.origin.x
-            println("\(position)")
+            println("The position is \(position)")
             
-            if position < -240 {
+            hideAllIcons()
+            
+            if position < -260 {
                 
                 /*----------------ADD TO LIST------------------*/
                 mailView.backgroundColor = brown
+                listIconImage.alpha = 1
+                listIconImage.frame.origin.x = position + 340
                 
-            } else if position < -50 {
+            } else if position < -60 {
                 
                 /*----------------READ LATER------------------*/
                 mailView.backgroundColor = yellow
+                listIconImage.alpha = 0
+                laterIconImage.alpha = 1
+                laterIconImage.frame.origin.x = position + 340
                 
-            } else if position > -50 && position < 0 {
+            } else if position > -60 && position < 0 {
                 
-                /*----------------ADD TO LIST grey'd out------------------*/
+                /*----------------READ LATER grey'd out------------------*/
                 mailView.backgroundColor = grey
+                laterIconImage.frame.origin.x = 276
+                var laterAlpha = convertValue(Float(position), r1Min: -60, r1Max: -30, r2Min: 1, r2Max: 0)
+                laterIconImage.alpha = CGFloat(laterAlpha)
                 
-            } else if position > 240 {
+                
+            } else if position > 260 {
             
                 /*----------------DELETE------------------*/
                 mailView.backgroundColor = red
+                deleteIconImage.alpha = 1
+                deleteIconImage.frame.origin.x = position - 40
                 
-            } else if position > 50 {
+            } else if position > 60 {
                 
                 /*----------------ARCHIVE------------------*/
                 mailView.backgroundColor = green
+                archiveIconImage.alpha = 1
+                archiveIconImage.frame.origin.x = position - 40
                 
-            } else if position < 50 && position > 0 {
+            } else if position < 60 && position > 0 {
                 
                 /*----------------ARCHIVE grey'd out------------------*/
                 mailView.backgroundColor = grey
+                archiveIconImage.frame.origin.x = 20
+                var laterArchive = convertValue(Float(position), r1Min: 30, r1Max: 60, r2Min: 0, r2Max: 1)
+                archiveIconImage.alpha = CGFloat(laterArchive)
+                println("the archive's alpha is \(laterArchive)")
                 
             }
             
@@ -102,36 +123,50 @@ class ViewController: UIViewController {
             var position = mailImage.frame.origin.x
             println("\(position)")
             
-            if position < -240 {
+            if position < -260 {
                 
                 /*----------------ADD TO LIST------------------*/
-                mailImage.frame.origin.x = 0
+                UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                    self.finishOffLeft()
+                    }, completion: nil)
                 
-            } else if position < -50 {
+            } else if position < -60 {
                 
                 /*----------------READ LATER------------------*/
-                mailImage.frame.origin.x = 0
                 
-            } else if position > -50 && position < 0 {
+                UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                    self.finishOffLeft()
+                }, completion: nil)
                 
-                /*----------------ADD TO LIST grey'd out------------------*/
-                mailImage.frame.origin.x = 0
                 
-            } else if position > 240 {
+            } else if position > -60 && position < 0 {
+                
+                /*----------------READ LATER grey'd out------------------*/
+                
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    self.mailImage.frame.origin.x = 0
+                })
+                
+            } else if position > 260 {
                 
                 /*----------------DELETE------------------*/
-                mailImage.frame.origin.x = 0
+                UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                    self.finishOffRight()
+                    }, completion: nil)
                 
-            } else if position > 50 {
+            } else if position > 60 {
                 
                 /*----------------ARCHIVE------------------*/
-                mailImage.frame.origin.x = 0
+                UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                    self.finishOffRight()
+                    }, completion: nil)
                 
-            } else if position < 50 && position > 0 {
+            } else if position < 60 && position > 0 {
                 
                 /*----------------ARCHIVE grey'd out------------------*/
-                mailImage.frame.origin.x = 0
-                
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    self.mailImage.frame.origin.x = 0
+                })
             }
 
             
@@ -141,9 +176,69 @@ class ViewController: UIViewController {
     }
     
     
+    func finishOffLeft() {
+        var position = mailImage.frame.origin.x
+        
+        UIView.animateWithDuration(0.3, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+            self.mailImage.frame.origin.x -= 320
+            self.listIconImage.frame.origin.x -= 320
+            self.laterIconImage.frame.origin.x -= 320
+            }) { (finshed: Bool) -> Void in
+                
+                self.resizeScrollView()
+                
+        }
+        
+        
+        
+    }
     
     
+    func finishOffRight() {
+        var position = mailImage.frame.origin.x
+        
+        UIView.animateWithDuration(0.3, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+            self.mailImage.frame.origin.x += 320
+            self.archiveIconImage.frame.origin.x += 320
+            self.deleteIconImage.frame.origin.x += 320
+            }) { (finshed: Bool) -> Void in
+                
+                self.resizeScrollView()
+                
+        }
+        
+        
+        
+    }
     
+    
+    func resizeScrollView() {
+        
+        var navHeight = navBarImage.image!.size.height
+        var helpHeight = helpImage.image!.size.height
+        var searchHeight = searchImage.image!.size.height
+        var mailHeight = mailView.frame.size.height
+        var feedHeight = feedImage.image!.size.height
+        
+        UIView.animateWithDuration(1, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.2, options: nil, animations: { () -> Void in
+            self.feedImage.frame.origin.y -= 86
+            self.myScrollView.contentSize = CGSizeMake(320, navHeight + helpHeight + searchHeight + feedHeight)
+            println("finshed")
+            }, completion: nil)
+    }
+    
+    func hideAllIcons() {
+        laterIconImage.alpha = 0
+        listIconImage.alpha = 0
+        archiveIconImage.alpha = 0
+        deleteIconImage.alpha = 0
+    }
+    
+    
+    func convertValue(value: Float, r1Min: Float, r1Max: Float, r2Min: Float, r2Max: Float) -> Float {
+        var ratio = (r2Max - r2Min) / (r1Max - r1Min)
+        return value * ratio + r2Min - r1Min * ratio
+    }
     
     
     
